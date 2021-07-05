2ef4e1d553e27c17ecb54c7f5c0e61c27f8ec1b6
jupemara <jumpei.arashi@arashike.com>
2021-07-05 23:18:51 +0900

    sample test case with matrix test method

--- interface-as-specification/app/usecase_test.go
+++ interface-as-specification/app/usecase_test.go
@@ -1,1 +1,30 @@
 package app_test
+
+import (
+	"testing"
+
+	"github.com/jupemara/go-samples/interface-as-specification/app"
+	"github.com/jupemara/go-samples/interface-as-specification/repository"
+)
+
+func TestUserFindUsecase(t *testing.T) {
+	kvs := map[string]struct {
+		Id      string
+		IsError bool
+	}{
+		"exists user":     {"user001", false},
+		"non-exists user": {"not-exists", true},
+	}
+	for k, v := range kvs {
+		usecase := app.NewUsecase(
+			repository.NewMemory(),
+		)
+		err := usecase.Execute(v.Id)
+		if v.IsError && err == nil {
+			t.Fatalf(`case: %s. this case shuld be occurr error.`, k)
+		}
+		if !v.IsError && err != nil {
+			t.Fatalf(`case: %s. unexpected error occurred: %s`, k, err)
+		}
+	}
+}
